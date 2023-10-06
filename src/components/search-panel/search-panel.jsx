4d2e2ce888acf8e93af266835b79907229af4d8b
Dmitrii Privezencev <dv.privezencev@mail.ru>
2023-10-07 00:39:18 +0300

    finish

--- src/components/search-panel/search-panel.jsx
+++ src/components/search-panel/search-panel.jsx
@@ -1,12 +1,35 @@
+import { Component } from 'react';
 import './style.css'
 
 
-function SearchPanel() {
-	return (
-		<div>
-			<input type="text" className='form-control search-input' placeholder='Найти сотрудника'/>
-		</div>
-	);
+class SearchPanel extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			term: ''
+		}
+	}
+
+	onUpdateSearch = (e) => {
+		const term = e.target.value;
+		this.setState({ term });
+		this.props.onUpdateSearch(term)
+	}
+
+	render() {
+		return (
+			<div>
+				<input
+					type="text"
+					className='form-control search-input'
+					placeholder='Найти сотрудника'
+					value={this.state.term}
+					onChange={this.onUpdateSearch}
+				/>
+			</div>
+		);
+	}
+
 }
 
 export default SearchPanel;
