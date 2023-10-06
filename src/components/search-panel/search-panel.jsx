0e553f97784ddb4e0e08bf7459258a7986c61244
Dmitrii Privezencev <dv.privezencev@mail.ru>
2023-10-06 19:51:04 +0300

    layout

---
+++ src/components/search-panel/search-panel.jsx
@@ -0,0 +1,12 @@
+import './style.css'
+
+
+function SearchPanel() {
+	return (
+		<div>
+			<input type="text" className='form-control search-input' placeholder='Найти сотрудника'/>
+		</div>
+	);
+}
+
+export default SearchPanel;
